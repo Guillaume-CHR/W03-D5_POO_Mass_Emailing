5ab5c315dfd20bd8d94fa296d80c9ffd4f5ba548
guillaume-CHR <guillaume.christe@gmail.com>
2019-01-25 16:17:19 +0100

    app

--- app.rb
+++ app.rb
@@ -1,14 +1,14 @@
 #! /usr/bin/env ruby
 ##****************************************************************************
-# RUBY - TIC TAC TOE
+# RUBY - Mass Emailing
 #****************************************************************************
 #   Ruby's Program - Main Program
 #   Written by: THP Tls session 7
 # 	Date: 25-Jan-2019
 #   
 #   Description:
-# 		- Program to simulate a Tic Tac Toe game
-# 		- It uses the MVC method ... or tries to ...
+# 		- 
+# 		- 
 # 		
 # 	Gems:
 		require 'bundler'
